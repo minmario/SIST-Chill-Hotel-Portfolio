2999a5779239ebe4ece3b8f9c1d9ddd3e1db0db0
ted19horse <153588307+ted19horse@users.noreply.github.com>
2025-04-05 05:42:01 +0900

    api connect test

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -21,6 +21,15 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ];
+  },
 }
 
 mergeConfig(nextConfig, userConfig)
