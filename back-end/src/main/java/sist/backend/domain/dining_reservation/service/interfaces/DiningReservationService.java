c45a6e92b1f276a9a2d97f662cffe1c82d48a5dc
Graz1e <fprhelwkdlsj@naver.com>
2025-04-09 00:29:17 +0900

    desktop_back

--- back-end/src/main/java/sist/backend/domain/dining_reservation/service/interfaces/DiningReservationService.java
+++ back-end/src/main/java/sist/backend/domain/dining_reservation/service/interfaces/DiningReservationService.java
@@ -1,5 +1,29 @@
 package sist.backend.domain.dining_reservation.service.interfaces;
 
+import lombok.RequiredArgsConstructor;
+import sist.backend.domain.dining_reservation.entity.DiningReservation;
+import sist.backend.domain.dining_reservation.repository.jpa.DiningReservationRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class DiningReservationService {
 
+    private final DiningReservationRepository reservationRepository;
+
+    public DiningReservation save(DiningReservation reservation) {
+        reservation.setReservationNum(generateUniqueCode());
+        return reservationRepository.save(reservation);
+    }
+
+    private String generateUniqueCode() { // UUid를 사용하여 고유한 예약 번호 생성
+        String code;
+        do {
+            code = "RS" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        } while (reservationRepository.existsByReservationNum(code));
+        return code;
+    }
 }
