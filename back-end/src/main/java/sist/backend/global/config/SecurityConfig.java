59da854dfeda6f379904087fdf4539499fe905c6
Ubuntu <ubuntu@ip-172-31-9-91.ap-northeast-2.compute.internal>
2025-05-23 09:18:39 +0000

    https sc

--- back-end/src/main/java/sist/backend/global/config/SecurityConfig.java
+++ back-end/src/main/java/sist/backend/global/config/SecurityConfig.java
@@ -83,6 +83,120 @@ public class SecurityConfig {
                 "http://43.203.34.21:8080",
                 "http://3.39.138.132:3000",
                 "http://3.39.138.132:8080",
+                package sist.backend.global.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+
+import lombok.RequiredArgsConstructor;
+import sist.backend.global.security.JwtAuthenticationFilter;
+import sist.backend.global.jwt.JwtProvider;
+
+import java.util.Arrays;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtProvider, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // .requestMatchers("/admin/**").hasAnyRole("ADMIN", "STAFF")
+                        // .requestMatchers("/api/admin/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers("/admin/**").permitAll()
+                        .requestMatchers("/api/admin/**").permitAll()
+                        .requestMatchers("/api/dining/**").permitAll()
+                        .requestMatchers("/api/restaurants/**").permitAll()
+                        .requestMatchers("/api/mypage/**").authenticated()
+                        .requestMatchers(
+                                "/api/user/auth/logout",
+                                "/api/mypage/**",
+                                "/api/user/stays/summary",
+                                "/api/user/summary/update",
+                                "/api/user/points/summary",
+                                "/api/user/points",
+                                "/api/user/me")
+                        .authenticated()
+                        .anyRequest().permitAll())
+                // ?”¥ ?¬ê¸°!! formLogin(), logout() **?„ì „???? œ**
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "http://3.37.123.6:3000",
+                "http://3.37.123.6:8080",
+                "http://43.203.34.21:3000",
+                "http://43.203.34.21:8080",
+                "http://3.39.138.132:3000",
+                "http://3.39.138.132:8080",
+                "https://sistchillhotel.com",
+                "https://www.sistchillhotel.com"
+          
+                
+                
+                
+                "*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
+        configuration.setAllowedHeaders(
+                Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
+        configuration
+                .setExposedHeaders(Arrays.asList("Authorization", "Content-Disposition", "Content-Type", "Set-Cookie"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
                 
                 
                 
