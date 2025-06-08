75f6a2198d18abd9c2506745dd7f420a8ea39f08
Ubuntu <ubuntu@ip-172-31-9-91.ap-northeast-2.compute.internal>
2025-06-08 18:04:46 +0000

    e4

--- back-end/src/main/java/sist/backend/global/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/sist/backend/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletRequest; 
 
 import java.util.HashMap;
 import java.util.Map;
